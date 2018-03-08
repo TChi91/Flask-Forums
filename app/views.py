8adbed12e417eca1c366b51948acb20920e05b34
Fethi TChi <adm4algnw@gmail.com>
2018-03-09 00:08:25 +0100

    fix error CRUD workshop

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request,redirect,url_for
+from flask import render_template, request,redirect,url_for, abort
 from app import models
 from app import app, member_store, post_store
 
@@ -21,20 +21,32 @@ def add_topic():
 
 @app.route("/topic/delete/<int:id>")
 def delete_topic(id):
-    post_store.delete(id)
+    try:
+        post_store.delete(id)
+    except ValueError:
+        abort(404)
     return redirect(url_for('home'))
 
 @app.route("/topic/show/<int:id>")
 def show_topic(id):
     post = post_store.get_by_id(id)
+    if post is None:
+        abort(404)
     return render_template("show.html", post = post)
 
 @app.route("/topic/edit/<int:id>", methods=['GET', 'POST'])
 def edit_topic(id):
     post = post_store.get_by_id(id)
+    if post is None:
+        abort(404)
     if request.method == 'POST':
         post.title = request.form['title']
         post.content = request.form['content']
+        post_store.update(post)
         return redirect(url_for('home'))
     else:
         return render_template("edit.html", post = post)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
