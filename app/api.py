6e78f1207b7067b27e7e7cebd4da9f63a6536c56
Fethi TChi <adm4algnw@gmail.com>
2018-03-13 10:40:43 +0100

    errorhandler and static files improvement

--- app/api.py
+++ app/api.py
@@ -1,4 +1,4 @@
-from flask import render_template, request,redirect,url_for, abort, jsonify
+from flask import request, abort, jsonify, render_template
 from app import models
 from app import app, member_store, post_store
 
@@ -13,34 +13,54 @@ def all_apis():
 @app.route("/api/topic/show/<int:id>")
 def show_api(id):
     post = post_store.get_by_id(id)
-    if post is None:
-        abort(404)
-    return jsonify(post.__dict__())
+    try:
+        result = jsonify(post.__dict__())
+    except AttributeError:
+        result = abort(404, f"Topic with id: {id} doesn't exist")
+    return result
 
 
 @app.route("/api/topic/add", methods=['POST'])
 def add_api():
     request_data = request.get_json()
-    new_post = models.Post(request_data['title'], request_data['content'])
-    post_store.add(new_post)
-    return jsonify(new_post.__dict__())
+    try:
+        new_post = models.Post(request_data['title'], request_data['content'])
+        post_store.add(new_post)
+        result = jsonify(new_post.__dict__())
+    except KeyError:
+        result = abort(400, f"Couldn't parse the request data !")
+    return result
 
 
 @app.route("/api/topic/delete/<int:id>", methods=['DELETE'])
 def delete_api(id):
     post = post_store.get_by_id(id)
-    if post is None:
-        abort(404)
-    post_store.delete(id)
-    return jsonify(post.__dict__())
+    try:
+        post_store.delete(id)
+        result = jsonify(post.__dict__())
+    except AttributeError:
+        result = abort(404, f"Topic with id: {id} doesn't exist")
+    except ValueError:
+        result = abort(404, f"Topic with id: {id} doesn't exist")
+    return result
+
 
 @app.route("/api/topic/update/<int:id>", methods=['PUT'])
 def edit_api(id):
     post = post_store.get_by_id(id)
-    if post is None:
-        abort(404)
     request_data = request.get_json()
-    post.title = request_data['title']
-    post.content = request_data['content']
-    post_store.update(post)
-    return jsonify(post.__dict__())
+    try:
+        post.title = request_data['title']
+        post.content = request_data['content']
+        post_store.update(post)
+        result = jsonify(post.__dict__())
+    except AttributeError:
+        result = abort(404, f"Topic with id: {id} doesn't exist")
+    except KeyError:
+        result = abort(400, f"Couldn't parse the request data !")
+    return result
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify(message = error.description)
--- app/views.py
+++ app/views.py
@@ -9,6 +9,14 @@ def home():
     return render_template("index.html", posts = posts)
 
 
+@app.route("/topic/show/<int:id>")
+def show_topic(id):
+    post = post_store.get_by_id(id)
+    if post is None:
+        abort(404, f"topic with id: {id} doesn't exist.")
+    return render_template("show.html", post = post)
+
+
 @app.route("/topic/add", methods=['GET', 'POST'])
 def add_topic():
     if request.method == 'POST':
@@ -24,21 +32,15 @@ def delete_topic(id):
     try:
         post_store.delete(id)
     except ValueError:
-        abort(404)
+        abort(404, f"topic with id: {id} doesn't exist.")
     return redirect(url_for('home'))
 
-@app.route("/topic/show/<int:id>")
-def show_topic(id):
-    post = post_store.get_by_id(id)
-    if post is None:
-        abort(404)
-    return render_template("show.html", post = post)
 
 @app.route("/topic/edit/<int:id>", methods=['GET', 'POST'])
 def edit_topic(id):
     post = post_store.get_by_id(id)
     if post is None:
-        abort(404)
+        abort(404, f"Topic with id: {id} doesn't exist.")
     if request.method == 'POST':
         post.title = request.form['title']
         post.content = request.form['content']
@@ -48,5 +50,5 @@ def edit_topic(id):
         return render_template("edit.html", post = post)
 
 @app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+def page_not_found(error):
+    return render_template('404.html', message = error.description)
