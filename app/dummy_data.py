0b2369afe08e6900efb473a3eee19f3fca04ea89
Fethi TChi <adm4algnw@gmail.com>
2018-04-06 23:06:35 +0100

    Update dummy_data.py

--- app/dummy_data.py
+++ app/dummy_data.py
@@ -30,7 +30,7 @@ def seed_stores(member_store, post_store):
        db.drop_all()
        db.create_all()
 
-       for member in dummy_members:
-           member_store.add(member)
-       for post in dummy_posts:
-           post_store.add(post)
+   for member in dummy_members:
+       member_store.add(member)
+   for post in dummy_posts:
+       post_store.add(post)
